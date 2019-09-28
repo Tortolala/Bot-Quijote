a5a8291c13e4cf43bf428dbbf8ee40a2fc709207
Luis Angel <tortoladev@gmail.com>
2019-09-28 01:13:52 -0600

    Http request and data binding for UI

--- BotQuijoteUI/src/app/app.component.ts
+++ BotQuijoteUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BotQuijoteUI';
+
+  title = 'Bot Quijote';
+  baseUrl: string = 'http://127.0.0.1:5000'
+  seed: string;
+  length: number;
+  prediction: string;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getPrediction() {
+    this.httpClient.get(this.baseUrl + '/predict/' + this.seed + '/' + this.length.toString()).subscribe((res) => {
+      this.prediction = res.toString();
+    });
+  }
+
 }
