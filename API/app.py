b688214d96276f89b3b4927ac2f692e8cbbf7377
Luis Angel <tortoladev@gmail.com>
2019-09-27 18:54:42 -0600

    Flask API created with dummy prediction function

---
+++ API/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def predict_text(seed, length):
+
+    dummy_prediction = 'a'*length
+    text = seed + dummy_prediction
+
+    return text
+
+@app.route("/")
+def hello():
+    return "Hola, I'm Bot Quijote de la Mancha."
+
+@app.route("/predict/<string:seed>/<int:length>", methods=["GET"])
+def predict(seed, length):
+    predicted_text = predict_text(seed, length)
+    return predicted_text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bot-Quijote
+
+This repository is part of a project for the AI course at uni.
+
+This repository houses an API and a webpage used to showcase the result of the trained RNN model. The model can be found in its own [repository](https://github.com/ricardojmv85/project1_ai).
